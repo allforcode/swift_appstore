3f00ba205b8f9c15d19b90cfc69838501b64add2
Paul Dong <paul.d@strakertranslations.com>
2017-10-08 08:46:48 +1300

    lesson 1

---
+++ appstore/FeaturedAppsController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  appstore
+//
+//  Created by Paul Dong on 7/10/17.
+//  Copyright © 2017 Paul Dong. All rights reserved.
+//
+
+import UIKit
+
+class FeaturedAppsController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    private let cellId = "cellId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = UIColor.white
+        collectionView?.register(CategoryCell.self, forCellWithReuseIdentifier: cellId)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! CategoryCell
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 230)
+    }
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        return 0
+//    }
+}
